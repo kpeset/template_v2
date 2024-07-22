50ea9c761fbad919f5876e364aad4b80549c64ba
Kevin Peset <kpeset@Air-de-Kevin.fritz.box>
2024-07-22 16:55:42 +0200

    Modification React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Mon template</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda sunt
+        quisquam est nulla repudiandae ullam adipisci cupiditate modi non,
+        recusandae corporis corrupti blanditiis voluptates perspiciatis
+        voluptatibus natus ipsa iusto eos?
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
